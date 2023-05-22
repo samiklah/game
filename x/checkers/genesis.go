1bf9f3b588c6a9e501fbbde5ca7687c7d063a96d
samiklah <samikla7@gmail.com>
2023-05-22 23:30:38 +0300

    add

--- x/checkers/genesis.go
+++ x/checkers/genesis.go
@@ -13,6 +13,10 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 	if genState.SystemInfo != nil {
 		k.SetSystemInfo(ctx, *genState.SystemInfo)
 	}
+	// Set all the storedGame
+	for _, elem := range genState.StoredGameList {
+		k.SetStoredGame(ctx, elem)
+	}
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
 }
@@ -20,13 +24,13 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 // ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
-	genesis.Params = k.GetParams(ctx)
 
 	// Get all systemInfo
 	systemInfo, found := k.GetSystemInfo(ctx)
 	if found {
 		genesis.SystemInfo = &systemInfo
 	}
+	genesis.StoredGameList = k.GetAllStoredGame(ctx)
 	// this line is used by starport scaffolding # genesis/module/export
 
 	return genesis
